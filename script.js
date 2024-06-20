4b3ab4aa019e86e4bd6c7ae064cb2ce31951a22b
mmccjjj <dhuber88@sunrise.ch>
2024-06-20 10:50:19 +0200

    jetzt

--- script.js
+++ script.js
@@ -4,16 +4,24 @@ let bSmile= document.getElementById("smileBad")
 
 
 /* funktion die die Farbe des emojis ändert und den text für eine positive Bewertung einfügt */
+/* funktion so angepasst dass man zwischen positiv und negativ springen kann */
 
 function good(){
+nSmile.classList.remove("notShow");
+bSmile.classList.remove("show");
+nSmile.classList.add("show");
+bSmile.classList.add("notShow");
 nSmile.style.color= "green";
 alert.innerText= ("Thank You");
 alert.style.color= "green";
 
 }
 /* funktion die neutralen emoji ausblendet (füt klasse hinzu) und negativen emoji einblendet und text hinzufügt für eine negative bewertung und farbe anpasst */
+/* funktion so angepasst dass man zwischen positiv und negativ springen kann */
 
 function bad(){
+nSmile.classList.remove("show");
+bSmile.classList.remove("notShow");
 nSmile.classList.add("notShow");
 bSmile.classList.add("show");
 bSmile.style.color= "red";
